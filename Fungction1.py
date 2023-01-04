2cb74e643c707c928f8457f375eb6a9c3806bb7e
rahellianatinambunan <116257323+rahellianatinambunan@users.noreply.github.com>
2023-01-04 21:57:25 +0700

    Add files via upload

---
+++ Fungction1.py
@@ -0,0 +1,12 @@
+def sapa_mawar():
+      print("Hai Mawar");
+ 
+def sapa_Rafael():
+  print("Morning, Rafael");
+ 
+def sapa_Steven():
+  print("Halo bro,..");
+ 
+sapa_mawar()
+sapa_Rafael()
+sapa_Steven()
